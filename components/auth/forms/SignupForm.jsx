17a5faf8c91ba96505a91ab1533d7b1d92b26d52
drewkeat <drewjkeat@gmail.com>
2023-08-06 01:31:28 -0600

    add formik validation to SignupForm

--- components/auth/forms/SignupForm.jsx
+++ components/auth/forms/SignupForm.jsx
@@ -1,20 +1,58 @@
 import {
   Box, TextField, FormControlLabel, Checkbox, Button
 } from "@mui/material"
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 
 export default function SignUpForm({children}){
-  
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
-  };
+
+  const signupSchema = yup.object({
+    firstName: yup.string().required(),
+    lastName: yup.string().required(),
+    email: yup.string().email('Invalid Email').required(),
+    password: yup.string().required(),
+    confirmPassword: yup.string().oneOf([yup.ref('password'),null],"Passwords must match").required('')  
+  })
+
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+    validationSchema: signupSchema,
+    onSubmit: (values) => console.log(values)
+  })
 
   return(
-    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+    <Box component="form" onSubmit={formik.handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            autoFocus
+            margin="normal"
+            required
+            fullWidth
+            id="firstname"
+            label="First name"
+            name="firstName"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={formik.touched.firstName && Boolean(formik.errors.firstName)}
+            helperText={formik.touched.firstName && formik.errors.firstName}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="lastName"
+            label="Last name"
+            name="lastName"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={formik.touched.lastName && Boolean(formik.errors.lastName)}
+            helperText={formik.touched.lastName && formik.errors.lastName}
+          />
           <TextField
             margin="normal"
             required
@@ -23,7 +61,10 @@ export default function SignUpForm({children}){
             label="Email Address"
             name="email"
             autoComplete="email"
-            autoFocus
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={formik.touched.email && Boolean(formik.errors.email)}
+            helperText={formik.touched.email && formik.errors.email}
           />
           <TextField
             margin="normal"
@@ -34,6 +75,24 @@ export default function SignUpForm({children}){
             type="password"
             id="password"
             autoComplete="current-password"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={formik.touched.password && Boolean(formik.errors.password)}
+            helperText={formik.touched.password && formik.errors.password}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="confirmPassword"
+            label="Confirm Password"
+            type="password"
+            id="confirmPassword"
+            autoComplete="current-password"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={formik.touched.confirmPassword && Boolean(formik.errors.confirmPassword)}
+            helperText={formik.touched.confirmPassword && formik.errors.confirmPassword}
           />
           <FormControlLabel
             control={<Checkbox value="remember" color="primary" />}
